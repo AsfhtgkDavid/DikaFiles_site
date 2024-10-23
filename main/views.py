9a18728aed5fe948d78744cefcb094789d7e49e8
AsfhtgkDavid <daika@i.ua>
2024-10-23 22:12:23 +0300

    Add download page

--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "main/index.html")
+
+
+def download(request):
+    return render(request, "main/download.html")
