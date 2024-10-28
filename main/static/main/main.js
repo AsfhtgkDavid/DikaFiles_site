da30b486f2af51b50b0a1046018a60c641423541
Some-O-ne <sasha.go.go.go.go@gmail.com>
2024-10-28 17:57:46 +0300

    actually fixes bug

--- main/static/main/main.js
+++ main/static/main/main.js
@@ -1,34 +1,41 @@
+
+import 'https://unpkg.com/@egjs/conveyer/dist/conveyer.min.js'
 let carousel = document.querySelector("div.imageCarousel")
+const conveyer = new Conveyer("div.imageCarousel");
+
 let images = [...carousel.children]
 let active = 0
 let isSwitchingManually = false
 
-// const scrollIntoViewHorizontally = (container,child) => {
-//     const child_offsetRight = child.offsetLeft + child.offsetWidth;
-//     const container_scrollRight = container.scrollLeft + container.offsetWidth;
-//     container.scrollLeft += 500
-//     container.scrollLeft = container.scrollLeft % 1500
-//     console.log(container.scrollLeft)
-//   };
-
-let scrollIntoViewIfNeeded;
-
-import(
-  'https://esm.sh/scroll-into-view-if-needed'
-).then(({default: scrollFunc}) => {scrollIntoViewIfNeeded = scrollFunc})
-  
 
 
 function nextImage(next) {
     active += next
-    if (active >= images.length) active = 0
-    if (active < 0) active = images.length-1
-    // carousel.scrollTo({
-    //     left: images[active].offsetLeft,
-    //     behavior: "smooth",
-    //   });
-    scrollIntoViewIfNeeded(images[active], { behavior: 'smooth', block: 'nearest', inline: 'center', scrollMode: 'if-needed' })
-    //scrollIntoViewHorizontally(carousel,images[active])
+    if (active >= images.length) {
+        active = 0;
+        conveyer.scrollIntoView("end", {
+            align: "center",
+            duration: 500,
+            excludeStand: true,
+          });
+        return
+    }
+    if (active < 0) {
+            active = images.length-1
+        conveyer.scrollIntoView("start", {
+            align: "center",
+            duration: 500,
+            excludeStand: true,
+          });
+          return
+    }
+    
+    conveyer.scrollIntoView(next == 1 && "next" || "prev", {
+        align: "center",
+        duration: 500,
+        excludeStand: true,
+      });
+
 }
 document.querySelectorAll("button[carouselButton]").forEach((button)=>{
     button.addEventListener("click",()=>{
