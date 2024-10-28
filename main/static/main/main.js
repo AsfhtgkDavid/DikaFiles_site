1660f8086ee6b74b4e9925244b09149ae2e2a524
Some-O-ne <sasha.go.go.go.go@gmail.com>
2024-10-28 19:54:14 +0300

    asd

--- main/static/main/main.js
+++ main/static/main/main.js
@@ -1,47 +1,47 @@
 
-import 'https://unpkg.com/@egjs/conveyer/dist/conveyer.min.js'
-let carousel = document.querySelector("div.imageCarousel")
-const conveyer = new Conveyer("div.imageCarousel");
+let carousel = document.querySelector("div.carouselWrapper")
 
-let images = [...carousel.children]
+
+let images = [...carousel.querySelectorAll("div.imageCarousel > img")]
+let dots = [...carousel.querySelectorAll("div.imageDots > p")]
 let active = 0
 let isSwitchingManually = false
-
+let timeout = null
 
 
 function nextImage(next) {
+    images[active].setAttribute("active",false);
+    dots[active].setAttribute("active",false);
     active += next
-    if (active >= images.length) {
-        active = 0;
-        conveyer.scrollIntoView("end", {
-            align: "center",
-            duration: 500,
-            excludeStand: true,
-          });
-        return
+    if (active >= images.length) {active = 0;}
+    if (active < 0) {active = images.length-1}
+    images[active].setAttribute("active",true);
+    dots[active].setAttribute("active",true);
+}
+
+function setSwitchingManually() {
+    isSwitchingManually = true
+    if (timeout) {
+        window.clearTimeout(timeout)
     }
-    if (active < 0) {
-            active = images.length-1
-        conveyer.scrollIntoView("start", {
-            align: "center",
-            duration: 500,
-            excludeStand: true,
-          });
-          return
-    }
-    
-    conveyer.scrollIntoView(next == 1 && "next" || "prev", {
-        align: "center",
-        duration: 500,
-        excludeStand: true,
-      });
+    timeout = window.setTimeout(()=>{isSwitchingManually =false},2000)
 
 }
+dots.forEach((dot)=>{
+    dot.addEventListener("click",()=>{
+        nextImage(Math.abs(active-dots.indexOf(dot)))
+        setSwitchingManually()
+    })
+
+})
+
 document.querySelectorAll("button[carouselButton]").forEach((button)=>{
     button.addEventListener("click",()=>{
         nextImage(Number(button.attributes.next.value))
-        isSwitchingManually = true
-        window.setTimeout(()=>{isSwitchingManually =false},2000)
+    setSwitchingManually()
+
+
+        
     })
 })
 window.setInterval(()=>{
